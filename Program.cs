e4e631e662d0df255cd16799b008f3e45462bd81
Huy <hqhuy1705@gmail.com>
2023-09-25 23:22:02 +0700

    Init Project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//using AlgorithmPractices.LeetCode;
+
+#region Easy
+
+// Two Sum
+//TwoSum.RunSolution();
+
+#endregion
+
+#region Medium
+
+// Add Two Numbers
+//AddTwoNumbers.RunSolution();
+
+#endregion
+
+#region Hard
+
+// FindMedianSortedArrays
+//FindMedianSortedArrays.RunSolution();
+
+#endregion
+
+Console.ReadLine();
